fe927d96c947d1415568349cb62610c67f1ac9a0
Jefson Oliveira <kaironoliveira16@gmail.com>
2023-08-22 22:08:01 -0400

    feat: fist config

---
+++ Amatec-Refrigeracao/src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <div>
+            <h1>Jefson & Marine</h1>
+        </div>
+    );
+}
+
+export default Header;
---
+++ Amatec-Refrigeracao/src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import Header from "../../components/Header";
+
+function Home() {
+  return (
+    <div>
+      <div>
+        <Header />
+      </div>
+      <div className="">
+        <h1>Marine & Jefson</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
