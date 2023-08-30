31e3787ded3be6ba4724c6356473fa7a13bed2f4
Jefson Oliveira <kaironoliveira16@gmail.com>
2023-08-29 22:06:02 -0400

    feat: Inclusão de Logo no Header e tambem na Aba da pagina

--- Amatec-Refrigeracao/src/components/Header/index.jsx
+++ Amatec-Refrigeracao/src/components/Header/index.jsx
@@ -1,8 +1,14 @@
+import Logo from "../../images/amatec1.png";
+
 function Header() {
   return (
-    <div className="bg-[#0f4583] p-9 max-h-9 flex justify-between items-center">
-      <div>
-        X-LOGO-X
+    <div className="bg-[#0f4583] p-9 max-h-10 flex justify-between items-center">
+      <div className="flex">
+        <img className="h-48 w-48" src={Logo} alt="" />
+        {/* <div className="">
+          <h1>Amatec Refrigeração</h1>
+          <h2>Assistência Técnica Especializada</h2>
+        </div> */}
       </div>
       <div className="flex font-serif font-semibold text-[18px] text-white">
         <ul className="">
